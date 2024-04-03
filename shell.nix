73d07b036c6ff6c1aa7e8d89ba76ca6dbd8c6708
bensejas <ben.sejas@canva.com>
2024-04-03 11:32:36 +1100

    Add build.sh

--- shell.nix
+++ shell.nix
@@ -4,26 +4,36 @@ let
   tarball = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/057f9aecfb71c4437d2b27d3323df7f93c010b7e.tar.gz";
     sha256 = "1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
+    # url = "https://github.com/NixOS/nixpkgs/archive/c8bc2f2c0d4abafe71132097976380e59d14c7c8.tar.gz";
+    # sha256 = "1yyl34qh6356rk2cyqprl0zgy5gw5v2vzmwkc9jsp825fvb34bbz";
   };
   nixpkgs = import tarball {};
 in
 with nixpkgs;
-clangStdenv.mkDerivation {
+mkShell {
   name = "canva-bazelbuild-intellij";
   buildInputs = [
     bazelisk
-    # jdk17
-    # gcc
+    bazel-buildtools
+    bazel
+    jdk11
+
+    gcc
     glib
     libcxx
-    # llvm
-    libclang
-    # libgcc
+    libstdcxx5
+    libgcc
+    glibc
+
+    llvm
+    # libclang
+    # glibc
+    # stdenv.cc.cc.lib
     # llvmPackages_13
     # clang
     # libstdcxx5
     # llvmPackages_14
     # lib
-    # glibc
   ];
+  # LD_LIBRARY_PATH = lib.makeLibraryPath [ stdenv.cc.cc ];
 }
