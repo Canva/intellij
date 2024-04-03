73c8b4b38d159d592ba9444264c0954355f93b9d
bensejas <ben.sejas@canva.com>
2024-04-03 11:37:40 +1100

    .

--- build.sh
+++ build.sh
@@ -5,11 +5,6 @@ set -eu -o pipefail
 
 REPO_DIR=$(git rev-parse --show-toplevel)
 
-# shellcheck source=tools/bash/_logging.sh
-source "${REPO_DIR}/tools/bash/_logging.sh"
-# shellcheck source=tools/observability/bash/_write_local_telemetry.sh
-source "${REPO_DIR}/tools/observability/bash/_write_local_telemetry.sh"
-
 main() {
   bazel build //ijwb:ijwb_bazel_zip --define=ij_product=intellij-oss-latest-stable
 }
