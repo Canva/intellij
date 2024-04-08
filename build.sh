63d17ee03312ab1d1c18e66a2aae974273ff3b89
bensejas <ben.sejas@canva.com>
2024-04-08 10:46:57 +1000

    Push plugin to depot

--- build.sh
+++ build.sh
@@ -7,7 +7,11 @@
 set -eu -o pipefail
 
 main() {
+  echo "--- Build Bazel Plugin"
   bazel build //ijwb:ijwb_bazel_zip --define=ij_product=intellij-oss-latest-stable
+  
+  echo "--- Push to Depot"
+  $DEPOT -b -f ./bazel-bin/ijwb/ijwb_bazel.zip -v 2023.3 -s canva -d bazelbuild-intellij-canva-shared-index.zip
 }
 
 main "$@"
