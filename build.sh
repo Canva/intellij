f88569abedf83bb0607587e9bc47db351f601d3e
bensejas <ben.sejas@canva.com>
2024-04-08 14:34:47 +1000

    remove extension

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ main() {
   bazel build //ijwb:ijwb_bazel_zip --define=ij_product=intellij-oss-latest-stable
   
   echo "--- Push to Depot"
-  $DEPOT -b -f ./bazel-bin/ijwb/ijwb_bazel.zip -v 2023.3 -s canva -d bazelbuild-intellij-canva-shared-index.zip
+  $DEPOT -b -f ./bazel-bin/ijwb/ijwb_bazel.zip -v 2023.3 -s canva -d bazelbuild-intellij-canva-shared-index
 }
 
 main "$@"
